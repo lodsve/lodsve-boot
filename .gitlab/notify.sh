a626065943e75f615f4ca63773e2f2aa71a59459
孙昊 <sunhao.java@gmail.com>
2024-06-11 09:48:07 +0000

    Update notify.sh

--- .gitlab/notify.sh
+++ .gitlab/notify.sh
@@ -13,4 +13,4 @@ MESSAGE="GitLab CI已完成执行，执行结果为：$RESULT！"
 # 示例：发送HTTP POST请求到钉钉机器人通知群
 curl -H "Content-Type: application/json" -X POST \
 -d '{"msgtype": "text", "text": {"content": "Lodsve '"$MESSAGE"'"}}' \
-https://oapi.dingtalk.com/robot/send?access_token=${env.DING_TOKEN}
+https://oapi.dingtalk.com/robot/send?access_token=${DING_TOKEN}
